5c92550118db31d5dfa1031655143684d9b97ce1
savva200197531 <kashin.savva@mail.ru>
2020-10-26 01:51:04 +0700

    finish word highlighting + search

--- rick_and_morty/rick_and_morty/fs_task.js
+++ rick_and_morty/rick_and_morty/fs_task.js
@@ -1,50 +1,53 @@
-window.addEventListener('DOMContentLoaded', () => {
+class Search {
+  constructor(allLinks, resultList, resultCounter) {
+    this.filteredLinks = []
+    this.counter = 0
+    this.allLinks = allLinks
+    this.resultList = resultList
+    this.resultCounter = resultCounter
+  }
 
+  findWord(event) {
+    const target = event.target
+    const filteredArr = Array.from(this.allLinks).filter(link => {
+      if (link.textContent.trim().toLowerCase().includes(target.value.trim().toLowerCase())) {
+        return link
+      }
+    })
+    if (target.value.trim().length > 0) {
+      filteredArr.forEach(link => {
+        let element = link.textContent
+        const reg = new RegExp(target.value.replace(/[-\/\\^$*+?.()|[\]{}]/g,'\\$&'), 'gi');
+        const result = element.replace(reg, str => `<span class="text-highlighted">${str}</span>`)
+        element = `<li><a href="${link.href}">${result}</a></li>`
+
+        this.filteredLinks.push(element)
+      })
+    } else {
+      this.resultList.textContent = ''
+    }
+    this.counter = this.filteredLinks.length
+    this.resultList.innerHTML = this.filteredLinks.join('')
+    if (this.counter > 0) {
+      this.resultCounter.textContent = `Найдено ${this.counter} совпадений`
+    } else {
+      this.resultCounter.textContent = 'Ничего не найдено'
+    }
+    this.filteredLinks = []
+  }
+}
+
+function loadContent() {
   const searchInput = document.querySelector('.search-input')
   const allLinks = document.querySelectorAll('a')
   const resultList = document.querySelector('.search-result')
   const resultCounter = document.querySelector('.result-counter')
 
-  class Search {
-
-    constructor() {
-      this.filteredLinks = []
-      this.counter = 0
-    }
-
-    findWord(event) {
-      const target = event.target
-      const filteredArr = Array.from(allLinks).filter(link => {
-        if (link.textContent.trim().toLowerCase().includes(target.value.trim().toLowerCase())) {
-          return link
-        }
-      })
-      if (target.value.trim().length > 0) {
-        filteredArr.forEach(link => {
-          let element = `<li><a href="${link.href}">${link.textContent}</a></li>`
-          if (element.trim().toLowerCase().includes(target.value.trim())) {
-            element = element.replace(target.value.trim(), `<span class="text-highlighted">${target.value.trim()}</span>`)
-          }
-          this.filteredLinks.push(element)
-        })
-      } else {
-        resultList.textContent = ''
-      }
-      this.counter = this.filteredLinks.length
-      resultList.innerHTML = this.filteredLinks.join('')
-      if (this.counter > 0) {
-        resultCounter.textContent = `Найдено ${this.counter} совпадений`
-      } else {
-        resultCounter.textContent = 'Ничего не найдено'
-      }
-      this.filteredLinks = []
-    }
-  }
-
-  const search = new Search()
-
+  const search = new Search(allLinks, resultList, resultCounter)
 
   searchInput.addEventListener('input', event => {
     search.findWord(event)
   })
-})
+}
+
+window.addEventListener('DOMContentLoaded', loadContent)
